3075148d4fdb16f6d6872cf8b019da5b96548a5e
Rashmica Gupta <rashmica.g@gmail.com>
2017-05-10 13:49:05 +1000

    more whitespace cleanups

--- htm_ctrl.c
+++ htm_ctrl.c
@@ -21,13 +21,13 @@ int htm_trigger (uint32_t i_target, int i_htm_type, uint64_t trigger)
 	uint64_t data = 0;
 
 	data |= trigger;
-        rc=htm_write_xscom(i_target, HTM_TRIG, i_htm_type, data);
-        if (rc) {
+	rc=htm_write_xscom(i_target, HTM_TRIG, i_htm_type, data);
+	if (rc) {
 		ERR("xscom HTM_MODE write failed, rc=%d\n", rc);
 		return -1;
 	}
 
-return 0;
+	return 0;
 }
 
 int htm_reset (uint32_t i_target, int i_htm_type)
@@ -38,9 +38,8 @@ int htm_reset (uint32_t i_target, int i_htm_type)
 
 int htm_set_marker(uint32_t i_target, int i_htm_type, uint64_t marker_val)
 {
-
-        /* And the Marker trigger to the value being added */
-        return htm_trigger(i_target, i_htm_type, (marker_val<<4 | CREATE_MARKER));
+	/* And the Marker trigger to the value being added */
+	return htm_trigger(i_target, i_htm_type, (marker_val<<4 | CREATE_MARKER));
 }
 
 int htm_start (uint32_t i_target, int i_htm_type)
@@ -62,22 +61,24 @@ int htm_stop (uint32_t i_target, int i_htm_type)
 	uint64_t data=0;
 	
 	rc=htm_trigger(i_target, i_htm_type, HTM_TRIG_STOP);
-	
+
 	do{
 		rc=htm_read_xscom(i_target, HTM_STAT, i_htm_type, &data);
-	        if (rc) {
+		if (rc) {
 			ERR("xscom HTM_ write failed, rc=%d\n", rc);
 			return -1;
 		}
 		sleep(1);
-	}while(!(data &= HTM_STAT_COMPLETE));
+	} while (!(data &= HTM_STAT_COMPLETE));
 
 	return 0;
 }
+
 int htm_pause (uint32_t i_target, int i_htm_type)
 {
 	return htm_trigger(i_target, i_htm_type, HTM_TRIG_PAUSE);
 }
+
 int htm_filter (bool filter, uint32_t i_target, int i_htm_type)
 {
 	int rc;
@@ -93,14 +94,13 @@ int htm_filter (bool filter, uint32_t i_target, int i_htm_type)
 		printf("filtering true \n");
 		data = SETFIELD(HTM_FILT, data, 0x0007f);
 		data = SETFIELD(HTM_FILT_TTAG, data, 0x02800);
-	}else{
-
+	} else {
 		data = SETFIELD(HTM_FILT, data, 0xfffff);
 	}
 	rc=htm_write_xscom(i_target, NHTM_FILT, i_htm_type, data);
-	 if (rc) {
-                        ERR("xscom NHTM_FILT read failed, rc=%d\n", rc);
-                        return -1;
-        	 }
+	if (rc) {
+		ERR("xscom NHTM_FILT read failed, rc=%d\n", rc);
+		return -1;
+	}
 	return 0;
 }
--- htm_utils.c
+++ htm_utils.c
@@ -15,101 +15,103 @@
 #define MAX_TIMEOUT 5
 
 char *get_chip_dir(uint32_t i_cpu_id){
-        if(i_cpu_id==0)
-                return MEMTRACE_C0;
-        else if(i_cpu_id==1)
-                return MEMTRACE_C1;
-        else if(i_cpu_id==8)
-                return MEMTRACE_C8;
-        else if(i_cpu_id==16)
-                return MEMTRACE_C10;
-        else if(i_cpu_id==17)
-                return MEMTRACE_C11;
-        else
-                return "error";
+	if(i_cpu_id==0)
+		return MEMTRACE_C0;
+	else if(i_cpu_id==1)
+		return MEMTRACE_C1;
+	else if(i_cpu_id==8)
+		return MEMTRACE_C8;
+	else if(i_cpu_id==16)
+		return MEMTRACE_C10;
+	else if(i_cpu_id==17)
+		return MEMTRACE_C11;
+	else
+		return "error";
 }
 
 uint64_t get_mem_size(uint32_t i_target)
 {
-        uint64_t memsize, bytes_read = 0;
-        int fd;
-        char nbuf[strlen(MEMTRACE_DIR)+16];
-        char buf[20];
+	uint64_t memsize, bytes_read = 0;
+	int fd;
+	char nbuf[strlen(MEMTRACE_DIR)+16];
+	char buf[20];
 	char *chip_dir;
 	uint32_t cpu_id = i_target >> 4;
 	chip_dir = get_chip_dir(cpu_id);
-        snprintf(nbuf, sizeof(nbuf), "%s/%s/%s", MEMTRACE_DIR, chip_dir,"size");
+	snprintf(nbuf, sizeof(nbuf), "%s/%s/%s", MEMTRACE_DIR, chip_dir,"size");
 
-        fd = open(nbuf, O_RDONLY);
+	fd = open(nbuf, O_RDONLY);
 
-        if (fd < 0) {
-                perror("Failed to open size file");
-                exit(1);
-        }
+	if (fd < 0) {
+		perror("Failed to open size file");
+		exit(1);
+	}
 
-        bytes_read = read(fd, buf, 18);
+	bytes_read = read(fd, buf, 18);
 	if (! bytes_read) {
 		ERR("COULD NOT READ MEMORY SIZE\n");
 		exit(1);
 	}
-        memsize = strtoul(buf, NULL, 16);
-        return memsize;
+	memsize = strtoul(buf, NULL, 16);
+	return memsize;
 }
 
 uint64_t get_mem_start(uint32_t i_target)
 {
-        int fd;
+	int fd;
 	uint64_t bytes_read = 0;
-        char nbuf[strlen(MEMTRACE_DIR)+16];
-        char buf[20];
+	char nbuf[strlen(MEMTRACE_DIR)+16];
+	char buf[20];
 	char *chip_dir;
 	uint32_t cpu_id = i_target >> 4;
 
 	chip_dir = get_chip_dir(cpu_id);
-        snprintf(nbuf, sizeof(nbuf), "%s/%s/%s", MEMTRACE_DIR, chip_dir,"start");
-        
+	snprintf(nbuf, sizeof(nbuf), "%s/%s/%s", MEMTRACE_DIR, chip_dir,"start");
+
 	fd = open(nbuf, O_RDONLY);
 
-        if (fd < 0) {
-                perror("Failed to open file start file ");
-                exit(1);
-        }
+	if (fd < 0) {
+		perror("Failed to open file start file ");
+		exit(1);
+	}
 
-        bytes_read = read(fd, buf, 18);
+	bytes_read = read(fd, buf, 18);
 	if (! bytes_read) {
 		ERR("COULD NOT READ MEMORY SIZE\n");
 		exit(1);
 	}
 
-        return strtoull(buf, NULL, 16);
+	return strtoull(buf, NULL, 16);
 }
 
-uint64_t get_htm_last_reg(uint32_t i_target, int htm_type){
-        int rc;
-        uint64_t htm_last_data;
+uint64_t get_htm_last_reg(uint32_t i_target, int htm_type)
+{
+	int rc;
+	uint64_t htm_last_data;
 
-        rc=htm_read_xscom(i_target, HTM_LAST, htm_type, &htm_last_data);
+	rc=htm_read_xscom(i_target, HTM_LAST, htm_type, &htm_last_data);
 
-        if (rc) {
-                ERR("xscom HTM Status Register read_ex failed rc=%d\n", rc);
-        }
-        return htm_last_data;
-
-}
-
-uint64_t get_htm_status_reg(uint32_t i_target, int htm_type){
-        int rc;
-        uint64_t htm_status_data;
-        rc=htm_read_xscom(i_target, HTM_STAT,htm_type, &htm_status_data);
-        if (rc) {
-                ERR("xscom HTM Status Register read_ex failed rc=%d\n", rc);
-        }
-        return htm_status_data;
+	if (rc) {
+		ERR("xscom HTM Status Register read_ex failed rc=%d\n", rc);
+	}
+	return htm_last_data;
 
 }
 
-uint64_t get_htm_last_size(uint32_t i_target, int htm_type){
+uint64_t get_htm_status_reg(uint32_t i_target, int htm_type)
+{
+	int rc;
+	uint64_t htm_status_data;
+	rc=htm_read_xscom(i_target, HTM_STAT,htm_type, &htm_status_data);
+	if (rc) {
+		ERR("xscom HTM Status Register read_ex failed rc=%d\n", rc);
+	}
+	return htm_status_data;
 
+}
+
+uint64_t get_htm_last_size(uint32_t i_target, int htm_type)
+{
 	uint64_t start, last;
 	start = get_mem_start(i_target);
 	last = get_htm_last_reg(i_target, htm_type);
