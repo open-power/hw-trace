1317de521ff1180c176893a3d9f6c4560b0ece40
Rashmica Gupta <rashmica.g@gmail.com>
2017-05-18 15:18:12 +1000

    fix htm_allocate -- used to report a successful allocation even when the allocation actually failed, due to bytes_written being treated as unsigned in the check

--- htm_allocate.c
+++ htm_allocate.c
@@ -16,28 +16,28 @@
 
 int htm_allocate(uint64_t i_mem_size)
 {
-        int fd;
-	uint64_t bytes_written = 0;
-        char nbuf[strlen(MEMTRACE_DIR)+16];
-        char buf[20];
+	int fd;
+	ssize_t bytes_written = 0, bytes_to_write;
+	char nbuf[strlen(MEMTRACE_DIR)+16];
+	char buf[20];
 
 	printf("mem size %d\n", i_mem_size);
 
-	sprintf(buf,"%"PRIu64, i_mem_size);
-        snprintf(nbuf, sizeof(nbuf), "%s/%s", MEMTRACE_DIR, "enable");
-        fd = open(nbuf, O_WRONLY);
+	bytes_to_write = snprintf(buf, sizeof(buf), "%"PRIu64, i_mem_size);
+	snprintf(nbuf, sizeof(nbuf), "%s/%s", MEMTRACE_DIR, "enable");
+	fd = open(nbuf, O_WRONLY);
 
-        if (fd < 0) {
-                perror("Failed to open memtrace enable file");
-                exit(1);
-        }
+	if (fd < 0) {
+	perror("Failed to open memtrace enable file");
+	exit(1);
+	}
 
-        bytes_written = write(fd, buf, strlen(buf));
-	if (!bytes_written){
+	bytes_written = write(fd, buf, bytes_to_write);
+
+	if (bytes_written < bytes_to_write){
 		ERR("Error writing allocation size\n");
 		exit(1);
 	}
-	
 	printf("allocation successful\n");
         return 0;
 }
